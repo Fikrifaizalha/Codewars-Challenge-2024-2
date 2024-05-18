f395d8cf570461267aff67fde4b9009d04dafb20
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-18 19:24:51 +0700

    codewars

---
+++ 11-Convert a string to an array.js
@@ -0,0 +1,11 @@
+// Fikri Faizal Hamzah Alfa'iz //
+
+// Codewars Challenge 2024 //
+
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+function stringToArray(string) {
+  return string.split(" ");
+}
+console.log(stringToArray("Robin Singh"));
+console.log(stringToArray("I love arrays they are my favorite"));
---
+++ 12-Beginner Series #1 School Paperwork.js
@@ -0,0 +1,11 @@
+// Fikri Faizal Hamzah Alfa'iz //
+
+// Codewars Challenge 2024 //
+
+// https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+
+function paperwork(n, m) {
+  return n > 0 && m > 0 ? n * m : 0;
+}
+
+console.log(paperwork(5, 5));
